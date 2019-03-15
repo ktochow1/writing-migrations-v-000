3b0a5cf75c00014946faef72433cb29f4cbbaee6
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 20:09:54 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthday_to_students.rb
+++ db/migrate/02_add_grade_and_birthday_to_students.rb
@@ -3,7 +3,6 @@ class AddGradeAndBirthdayToStudents < ActiveRecord::Migration
   def change 
     add_column :students, :integer, :grade 
     add_column :students, :string, :birthdate
-    end 
   end
   
 end
